5456635f4d2ce4b3fba814138cd4404373a04624
parezcoydigo <chadblack1@gmail.com>
2012-12-27 15:25:51 -0500

    working on gevent crash

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def posts(urls=URLS):
     return render_template('students.html', feedList=feedList)            
     
 if __name__ == '__main__':
-#    app.run(debug=True)
-    port = int(os.environ.get('PORT',5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
+#   port = int(os.environ.get('PORT',5000))
+#   app.run(host='0.0.0.0', port=port)
 
