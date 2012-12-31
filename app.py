bf00a40f3fc90134487b0fd7def5b6ee42856513
parezcoydigo <chadblack1@gmail.com>
2012-12-31 13:40:45 -0500

    Update to feeds process.
    Updated feed.py, changed name of feeds.md to studentposts.md.
    Removed feed logic from app.py. Removed special studentposts route
    and its associated template.

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
-import os
-from feedparser import parse
 from flask import Flask, render_template
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.flatpages import FlatPages
 
 
-URLS = ['http://parezcoydigo.wordpress.com/feed', 'http://lawyersgunsmoneyblog.com/feed']
 DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
 FLATPAGES_EXTENSION = '.md'
@@ -16,21 +13,10 @@ Bootstrap(app)
 pages = FlatPages(app)
 
 
-def get_feed(url):
-    f=parse(url)
-    return f
-
 @app.route('/')
 def index():
     return render_template('base.html')
 
-@app.route('/studentposts/')
-def posts(urls=URLS):
-    feedList = {}
-    for url in urls:
-        feedList[url]=get_feed(url)
-    return render_template('students.html', feedList=feedList)            
-    
 @app.route('/<path:path>/')
 def page(path):
    page = pages.get_or_404(path)
