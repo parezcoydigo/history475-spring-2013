1d498e76a6cd11611010663423466024617d1c58
parezcoydigo <chadblack1@gmail.com>
2013-01-23 15:26:46 -0500

    added student feeds to feed.py.

--- feed.py
+++ feed.py
@@ -5,7 +5,8 @@ from feedparser import parse
 
 scriptPath  = os.path.dirname(sys.argv[0])
 
-urls=['http://parezcoydigo.wordpress.com/feed', 'https://americasouthandnorth.wordpress.com/feed/']
+urls=['http://parezcoydigo.wordpress.com/feed', 'http://rockytopranger.wordpress.com/feed', 'http://cbbucs09.wordpress.com/feed', 'http://athenasowl1.wordpress.com/feed', 'http://willpatterson1.wordpress.com/feed', 'http://moconn475.wordpress.com/feed','http://cshinklesr2013.wordpress.com/feed','http://nsprouse.wordpress.com/feed','http://mesohistoryblog.wordpress.com/feed','http://gohomecolonistsyouredrunk.wordpress.com/feed','http://gorillawarfare475.wordpress.com/feed','http://ecraig3.wordpress.com/feed','http://dymer3.wordpress.com/feed','http://igotoseekagreatperhaps1.wordpress.com/feed','http://mjones56.wordpress.com/feed','http://fantasmadecipactli.wordpress.com/feed','http://annawgreene.wordpress.com/feed','http://lasindigenas.wordpress.com/feed','http://mmcginn7.wordpress.com/feed','http://jrpoole92590.wordpress.com/feed','http://jperugin.wordpress.com/feed']
+
 today = datetime.datetime.now()
 
 def get_feed(url):
@@ -22,10 +23,12 @@ date: %d-%d-%d
 
 
 for feed in my_feeds:
-        output = output + "### " + feed.feed.title + "\n*  " + "[" + feed.entries[0].title + "](" + feed.entries[0].link + ")-- "\
-            + feed.entries[0].summary + "\n\n"
+    try:
+        output = output + "### " + feed.feed.title + "\n*  " + "[" + feed.entries[0].title + "](" + feed.entries[0].link + ")-- "+ feed.entries[0].summary + "\n\n"
+    except IndexError:
+        continue
 
 
-f = codecs.open(scriptPath+'/pages/studentposts.md', 'w', encoding='utf-8').write(output)
+f = codecs.open(scriptPath+'pages/studentposts.md', 'w', encoding='utf-8').write(output)
 print scriptPath
 print "All done!"
